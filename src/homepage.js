631b628ba96354f0fca8424a095f564171070e2a
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-01-07 12:52:45 -0600

    Created homepage.js

---
+++ src/homepage.js
@@ -0,0 +1,16 @@
+//homepage.js
+//By: Sam Schmitz
+
+import React from 'react';
+
+function Homepage() {
+	return (
+		<>
+		<div classname="homepage">
+			<p>Pokedex</p>
+		</div>
+		</>
+	);
+}
+
+export default Homepage;
