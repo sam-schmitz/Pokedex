7c9bf0a8116de886c323099e2674d6fbcd2fb587
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-01-07 14:06:08 -0600

    Added pokeapi-js-wrapper and got an example working with the homepage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Homepage from './homepage.js'
 
-function App() {
+const Pokedex = require("pokeapi-js-wrapper");
 
-    
+function App() {
+    const P = new Pokedex.Pokedex();
 
   return (
       <>
-      <div classname="App">
+      <div className="App">
         <Router>
             <Routes>
-                <Route path='/Pokedex' element={<Homepage />} />
+                <Route path='/Pokedex' element={
+                    <Homepage 
+                    Pokedex={P}
+                />} />
                 <Route path='/pokemon' element={<p>Pokemon</p>} />
             </Routes>
         </Router>
--- src/homepage.js
+++ src/homepage.js
@@ -1,13 +1,35 @@
 //homepage.js
 //By: Sam Schmitz
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-function Homepage() {
+function Homepage({Pokedex}) {
+	const [pokemon, setPokemon] = useState(null);   //state to store Pokemon data
+
+    useEffect(() => {
+        const fetchPokemonData = async () => {
+            try {
+                const data = await Pokedex.getPokemonByName("charizard");
+                setPokemon(data);   //update state with the fetched data
+            } catch (error) {
+                console.error("Error fetching Pokemon Data:", error);
+            }
+        };
+
+    fetchPokemonData();
+    }, []);
+
 	return (
 		<>
-		<div classname="homepage">
-			<p>Pokedex</p>
+		<div className="homepage">
+			<h1>Pokedex</h1>
+			{pokemon ? (
+                <p>
+                    <strong>Name:</strong> {pokemon.name}
+                </p>
+            ) : (
+                <p>Loading...</p>   //displayed while data is being fetched
+            )}
 		</div>
 		</>
 	);
