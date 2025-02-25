1b78a63f19770b76f4f8b48046f93bb6d3ff31b4
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-02-25 15:41:28 -0600

    Started work on adding evolution chains to pokemon pages

--- src/pokemonPage.js
+++ src/pokemonPage.js
@@ -7,13 +7,26 @@ import { useParams } from 'react-router-dom';
 function PokemonPage({Pokedex}) {
 	const id = useParams().id;
     const [pokemon, setPokemon] = useState(null);   //state to store Pokemon data
+    const [evolutions, setEvolutions] = useState([]); //list of evolution chains
 
 	useEffect(() => {
         const fetchPokemonData = async () => {
             try {
                 const data = await Pokedex.getPokemonByName(id);
+                const speciesData = await Pokedex.getPokemonSpeciesByName(id);
+                const evolutionChainId = speciesData.evolution_chain.url.split("/").slice(-2, -1)[0];
+                const evolutionData = await Pokedex.getEvolutionChainById(evolutionChainId);
+                const evolutionString = extractEvolutionNames(evolutionData.chain);
+                console.log(evolutionString);
                 setPokemon(data);   //update state with the fetched data
-                //console.log(data.types[0].type.name);
+
+                //turn the chain in evolutionData to an array with the names of the evolutions
+                /*
+                const evoChain = [];
+                for i in evoData:
+
+                */
+                setEvolutions(evolutionData);
             } catch (error) {
                 console.error("Error fetching Pokemon Data:", error);
             }
@@ -34,9 +47,23 @@ function PokemonPage({Pokedex}) {
             ) : (
                 <p>Loading...</p>   //displayed while data is being fetched
             )}
+                <p>
+                    <strong>Evolution Chain:</strong>{" "}
+                    {evolutions.length > 0 ? evolutions.join(", ") : "Loading..."}
+                </p>
 		</div>
 		</>
 	)
+}
+
+function extractEvolutionNames(chain, names = []) {
+    names.push(chain.species.name);
+
+    if (chain.evolves_to.length > 0) {
+        extractEvolutionNames(chain.evolves_to[0], names);
+    }
+
+    return names
 }
 
 export default PokemonPage
