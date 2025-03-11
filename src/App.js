42ca8c9995ba4a078fe5174bc5efe78853cfc821
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-03-11 14:45:03 -0500

    Added a component to change the tab title when the page is changed.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, {useState, useEffect } from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, useLocation} from 'react-router-dom';
 import Homepage from './homepage.js'
 import PokemonPage from './pokemonPage.js'
 
@@ -11,21 +11,51 @@ function App() {
 
   return (
       <>
-      <div className="App">
-        <Router>
-            <Routes>
-                <Route path='/Pokedex' element={
-                    <Homepage 
-                    Pokedex={P}
-                />} />
-                <Route path='/Pokedex/pokemon/:id' element={
-                    <PokemonPage 
-                    Pokedex={P}
-                />} />
-            </Routes>
-        </Router>
-      </div></>
+          <Router>
+              <PageTitleUpdater />
+              <div className="App">
+                  <Routes>
+                      <Route path='/Pokedex' element={
+                          <Homepage
+                              Pokedex={P}
+                          />} />
+                      <Route path='/Pokedex/pokemon/:id' element={
+                          <PokemonPage
+                              Pokedex={P}
+                          />} />
+                  </Routes>
+              </div>
+          </Router>
+      </>
   );
+}
+
+function PageTitleUpdater() {   //component to update tab title and favicon based on route
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.pathname === "/Pokedex") {
+            document.title = "Pokedex - Home";
+            updateFavicon("/favicon.ico");
+        } else if (location.pathname.startsWith("/Pokedex/pokemon/")) {
+            const pokemonName = location.pathname.split("/").pop();
+            document.title = `${capitalize(pokemonName)} - Pokedex`;
+        }
+    }, [location]);
+
+    return null;    //This component only performs side effects
+}
+
+function updateFavicon(url) {   //helper function to update the favicon dynamically
+    const favicon = document.querySelector("link[rel='icon']");
+    if (favicon) {
+        favicon.href = url;
+    }
+}
+
+
+function capitalize(str) {  //Helper function used to capitalize pokemon names
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 export default App;
