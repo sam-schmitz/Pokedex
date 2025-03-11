0f48d28d90f1ba72fc8a8f65ec0121e3003286a8
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-03-11 15:04:18 -0500

    Added move page to the router and pageTitleUpdater

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect } from 'react';
 import {BrowserRouter as Router, Route, Routes, useLocation} from 'react-router-dom';
 import Homepage from './homepage.js'
 import PokemonPage from './pokemonPage.js'
+import MovePage from './movePage.js';
 
 const Pokedex = require("pokeapi-js-wrapper");
 
@@ -23,6 +24,10 @@ function App() {
                           <PokemonPage
                               Pokedex={P}
                           />} />
+                      <Route path='/Pokedex/move/:id' element={
+                          <MovePage
+                              Pokedex={P}
+                          />} />
                   </Routes>
               </div>
           </Router>
@@ -40,6 +45,9 @@ function PageTitleUpdater() {   //component to update tab title and favicon base
         } else if (location.pathname.startsWith("/Pokedex/pokemon/")) {
             const pokemonName = location.pathname.split("/").pop();
             document.title = `${capitalize(pokemonName)} - Pokedex`;
+        } else if (location.pathname.startsWith("/Pokedex/move/")) {
+            const moveName = location.pathname.split("/").pop();
+            document.title = `${capitalize(moveName)} - Pokedex`;
         }
     }, [location]);
 
@@ -54,7 +62,7 @@ function updateFavicon(url) {   //helper function to update the favicon dynamica
 }
 
 
-function capitalize(str) {  //Helper function used to capitalize pokemon names
+function capitalize(str) {  //Helper function used to capitalize pokemon and move names
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
