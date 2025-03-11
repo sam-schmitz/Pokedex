5f4232b54c6a1e6788d2268538328704f69f987c
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-03-11 15:00:49 -0500

    Added a basic outline for MovePage.

---
+++ src/movePage.js
@@ -0,0 +1,17 @@
+// movePage.js
+// By: Sam Schmitz
+
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+function MovePage({ pokdex }) {
+    return (
+        <>
+            <div className="Move Page" >
+                <h1>Move Page</h1>
+            </div>
+        </>
+        )
+}
+
+export default MovePage
