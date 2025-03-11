ff83abafb225dbfc47587a6c3733db4792abdd3a
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-03-11 15:20:05 -0500

    updated MovePage to pull data from PokeAPI

--- src/movePage.js
+++ src/movePage.js
@@ -4,11 +4,38 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-function MovePage({ pokdex }) {
+function MovePage({ Pokedex }) {
+    const id = useParams().id;
+    const [move, setMove] = useState(null);
+
+    useEffect(() => {
+        const fetchMove = async () => {
+            try {
+                const data = await Pokedex.getMoveByName(id);
+                setMove(data);
+            } catch (error) {
+                console.error("Error fetching Move Data:", error);
+            }
+        };
+
+        fetchMove();
+    }, []);
+
     return (
         <>
             <div className="Move Page" >
-                <h1>Move Page</h1>
+                <h1>{id}</h1>
+                {move ? (
+                    <>
+                        <p>
+                            <strong>Power:</strong> {move.power} <br />
+                            <strong>Accuracy:</strong> {move.accuracy} <br />
+                            <strong>Type:</strong> {move.type.name}
+                        </p>
+                    </>
+                ) : (
+                    <p>Loading...</p>
+                )}
             </div>
         </>
         )
